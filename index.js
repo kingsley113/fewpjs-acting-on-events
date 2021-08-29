9a1bf0b5d0ec824ad52795091e4d44ff934cd618
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-08-28 22:59:48 -0700

    commit tree 61a66ca24a8b6cba0d43d363ba1d9891a1f9ddf8

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+let game = document.getElementById("game");
+
+dodger.style.backgroundColor = "#FF69B4";
+
+document.addEventListener("keydown", function (e) {
+	if (e.key === "ArrowLeft") {
+		moveDodgerLeft();
+	}
+
+	if (e.key === "ArrowRight") {
+		moveDodgerRight();
+	}
+
+});
+
+function moveDodgerLeft() {
+	let leftNumbers = dodger.style.left.replace("px", "");
+	let left = parseInt(leftNumbers, 10);
+
+	if (left > 0) {
+		dodger.style.left = `${left - 1}px`;
+	}
+	console.log(dodger.style.left);
+}
+
+function moveDodgerRight() {
+	let leftNumbers = dodger.style.left.replace("px", "");
+	let left = parseInt(leftNumbers, 10);
+
+	// if (left <= (game.offsetWidth - dodger.offsetWidth)) {
+	if (left < 360) {
+		dodger.style.left = `${left + 1}px`;
+	}
+	console.log(dodger.style.left);
+}
